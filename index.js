200bd08e1781a45c00eb5fe2a88c8b356ca4bc1c
YoshiBNE <yoshiyasu_express@me.com>
2022-03-15 11:02:28 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(obj, adjective) {
+    let arrayObj = [];
+    for (let i=0; i<obj.length; i++) {
+        // console.log(obj[i]);
+        let msg = `Thank you, ${obj[i]}, for the wonderful ${adjective} gift!`;
+        arrayObj.push(msg);
+        // console.log(arrayObj);
+    }
+    return arrayObj;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num = num - 1;
+    }
+}
